2de0e68eb2f942ff53aaf1b25265bc9cad1ce355
yoro1836 <95689076+yoro1836@users.noreply.github.com>
2025-02-08 20:41:20 +0900

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Suiya TP kernel by Yoro1836 (build date : DATE, version : DUMMY1)
+kernel.string=Suiya kernel by Yoro1836 (build date : DATE, version : DUMMY1)
 do.devicecheck=0
 do.modules=0
 do.systemless=0
